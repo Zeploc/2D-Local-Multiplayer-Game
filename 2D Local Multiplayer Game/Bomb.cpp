40a8c55438d36ba6b190ef561f2be941bd5d1799
MarximusPrime <39780696+MarximusPrime@users.noreply.github.com>
2018-10-04 13:41:12 +1300

    Bomb texture

--- 2D Local Multiplayer Game/Bomb.cpp
+++ 2D Local Multiplayer Game/Bomb.cpp
@@ -10,7 +10,7 @@
 Bomb::Bomb(glm::vec2 Position, Utils::EANCHOR _Anchor)
 	: Entity({ glm::vec3(Position, 1) ,{ 0, 0, 0 },{ 1, 1, 1 } }, _Anchor)
 {
-	const char* texture = "Resources/Images/OfficeBall.png";
+	const char* texture = "Resources/Images/Bomb.png";
 
 	std::shared_ptr<Plane> BombImage = std::make_shared<Plane>(Plane(0.5f, 0.5f, { 1.0f, 1.0f, 1.0f, 1.0f }, texture));
 	BombImage->bCullFace = false;
