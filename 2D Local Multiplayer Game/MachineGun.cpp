7eeb178a39757204dc06ff9b79dc88396fda4e8a
Dejavu1234 <JontyHoekstra@gmail.com>
2018-10-18 17:20:36 +1300

    Added music and sound cues

--- 2D Local Multiplayer Game/MachineGun.cpp
+++ 2D Local Multiplayer Game/MachineGun.cpp
@@ -2,13 +2,16 @@
 #include "Bullet.h"
 #include "Engine\SceneManager.h"
 #include "Engine\Scene.h"
+#include "Engine\SoundManager.h"
 #include "Player.h"
+
 
 
 MachineGun::MachineGun(glm::vec2 Position, Utils::EANCHOR _Anchor)
 	: Weapon(Position, _Anchor, MACHINE_GUN)
 {
-
+	SoundManager::GetInstance()->AddChannel("CGunfire");
+	SoundManager::GetInstance()->AddAudio("Resources/Sounds/BulletWhiz.mp3", false, "Shooting");
 }
 
 
@@ -19,7 +22,7 @@ MachineGun::~MachineGun()
 
 void MachineGun::Fire()
 {
-	
+	SoundManager::GetInstance()->PlayAudio("Shooting", "CGunfire");
 	std::shared_ptr<Bullet> BulletFromAGun = std::make_shared<Bullet>(Bullet({CurrentPlayer->transform}, Utils::CENTER,CurrentPlayer));
 	BulletFromAGun->Init(*body->GetWorld());
 	SceneManager::GetInstance()->GetCurrentScene()->AddEntity(BulletFromAGun);
